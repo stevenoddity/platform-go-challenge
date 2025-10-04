f25c3d6597f425683f14ed5b5f5475bb6f18a607
stefanos <steveofsam@gmail.com>
2025-10-04 23:10:45 +0300

    Integrate error handler and input validator.

--- services/favorite/favorite_service.go
+++ services/favorite/favorite_service.go
@@ -2,24 +2,24 @@ package favorite_service
 
 import (
 	"encoding/json"
-	"gwi/database"
 	favorite_model "gwi/models/favorite"
 	"net/http"
 	"strconv"
+
+	"gwi/database"
+	"gwi/utils"
 )
 
-// GET /favorites?user_id=1
 func GetFavorites(w http.ResponseWriter, r *http.Request) {
 	userIDStr := r.URL.Query().Get("user_id")
-
 	if userIDStr == "" {
-		http.Error(w, "Missing user_id", http.StatusBadRequest)
+		utils.SendError(w, utils.ErrBadRequest("Missing user_id"))
 		return
 	}
 
 	userID, err := strconv.Atoi(userIDStr)
 	if err != nil {
-		http.Error(w, "Invalid user_id", http.StatusBadRequest)
+		utils.SendError(w, utils.ErrBadRequest("Invalid user_id"))
 		return
 	}
 
@@ -30,43 +30,42 @@ func GetFavorites(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(userFavorites)
+	utils.SendSuccess(w, userFavorites)
 }
 
-// POST /favorites
 func AddFavorite(w http.ResponseWriter, r *http.Request) {
 	var newFav favorite_model.Favorite
 	if err := json.NewDecoder(r.Body).Decode(&newFav); err != nil {
-		http.Error(w, "Bad request", http.StatusBadRequest)
+		utils.SendError(w, utils.ErrBadRequest("Invalid JSON"))
 		return
 	}
 
-	// Assign new ID
+	if err := utils.ValidateFavorite(&newFav); err != nil {
+		utils.SendError(w, utils.ErrBadRequest(err.Error()))
+		return
+	}
+
 	newFav.ID = len(database.FavoritesDB) + 1
 	database.FavoritesDB = append(database.FavoritesDB, newFav)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(newFav)
+	utils.SendSuccess(w, newFav)
 }
 
-// DELETE /favorites/{id}
 func DeleteFavorite(w http.ResponseWriter, r *http.Request) {
 	idStr := r.URL.Path[len("/favorites/"):]
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		utils.SendError(w, utils.ErrBadRequest("Invalid ID"))
 		return
 	}
 
 	for i, f := range database.FavoritesDB {
 		if f.ID == id {
 			database.FavoritesDB = append(database.FavoritesDB[:i], database.FavoritesDB[i+1:]...)
-			w.WriteHeader(http.StatusNoContent)
+			utils.SendSuccess(w, nil)
 			return
 		}
 	}
 
-	http.Error(w, "Favorite not found", http.StatusNotFound)
+	utils.SendError(w, utils.ErrNotFound("Favorite not found"))
 }
